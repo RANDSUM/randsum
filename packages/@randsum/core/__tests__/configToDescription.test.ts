1fb4561de50969b96f56d7144a81f27b9b7a1c76
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-11-22 09:29:27 -0500

    Tests

--- packages/@randsum/core/__tests__/configToDescription.test.ts
+++ packages/@randsum/core/__tests__/configToDescription.test.ts
@@ -1,38 +1,33 @@
 import { describe, expect, test } from 'bun:test'
-import { createRollConfig } from './support/fixtures'
 import { configToDescription } from '../src/utils/configToDescription'
 
 describe('configToDescription', () => {
   test('returns an array strings matching the constraints', () => {
-    expect(
-      configToDescription(createRollConfig({ quantity: 1, sides: 100 }))
-    ).toEqual(['Roll 1 100-sided die'])
+    expect(configToDescription({ quantity: 1, sides: 100 })).toEqual([
+      'Roll 1 100-sided die'
+    ])
 
     expect(
-      configToDescription(
-        createRollConfig({
-          quantity: 2,
-          sides: 20,
-          modifiers: { add: 5, drop: { highest: 1 } }
-        })
-      )
+      configToDescription({
+        quantity: 2,
+        sides: 20,
+        modifiers: { add: 5, drop: { highest: 1 } }
+      })
     ).toEqual(['Roll 2 20-sided dice', 'Drop highest', 'Add 5'])
 
     expect(
-      configToDescription(
-        createRollConfig({
-          quantity: 200,
-          sides: 20,
-          modifiers: {
-            add: 2,
-            minus: 5,
-            drop: { highest: 1, lowest: 5 },
-            replace: { from: 1, to: 20 },
-            reroll: { greaterThan: 10, maxReroll: 2 },
-            unique: { notUnique: [1, 2, 3] }
-          }
-        })
-      )
+      configToDescription({
+        quantity: 200,
+        sides: 20,
+        modifiers: {
+          add: 2,
+          minus: 5,
+          drop: { highest: 1, lowest: 5 },
+          replace: { from: 1, to: 20 },
+          reroll: { greaterThan: 10, maxReroll: 2 },
+          unique: { notUnique: [1, 2, 3] }
+        }
+      })
     ).toEqual([
       'Roll 200 20-sided dice',
       'Drop highest',
@@ -43,13 +38,5 @@ describe('configToDescription', () => {
       'Add 2',
       'Subtract 5'
     ])
-
-    expect(
-      configToDescription(createRollConfig({ quantity: 2, sides: 10 }))
-    ).toEqual(['Roll 2 10-sided dice'])
-
-    expect(
-      configToDescription(createRollConfig({ quantity: 2, sides: 10 }))
-    ).toEqual(['Roll 2 10-sided dice'])
   })
 })
