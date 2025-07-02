e9ee196cc111eebcf34d511b9fae918bd7c6565f
alxjrvs@gmail.com <alxjrvs@gmail.com>
2025-07-02 19:13:31 -0400

    Extend

--- corePackages/dice/src/index.ts
+++ corePackages/dice/src/index.ts
@@ -2,12 +2,14 @@ export * from './D'
 
 export { roll } from './roll'
 
+export * from './guards'
+
+export type * from './types'
+
 export * from '@randsum/core'
 
-export * from './guards'
+export * from '@randsum/notation'
 
 export type * from '@randsum/core'
 
 export type * from '@randsum/notation'
-
-export type * from './types'
