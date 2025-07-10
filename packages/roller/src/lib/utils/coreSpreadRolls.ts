da2ad65e7a89decfeec152cbcdcbf9b56fa1b93b
alxjrvs@gmail.com <alxjrvs@gmail.com>
2025-07-10 00:50:05 -0400

    WIP

--- packages/roller/src/lib/utils/coreSpreadRolls.ts
+++ packages/roller/src/lib/utils/coreSpreadRolls.ts
@@ -2,13 +2,12 @@ import { coreRandom } from './coreRandom'
 import { generateNumericFaces } from './generateNumericFaces'
 
 export function coreSpreadRolls(quantity: number, max: number): number[] {
-  const facesArr = generateNumericFaces(max)
-
+  const faces = generateNumericFaces(max)
   const result = new Array<number>(quantity)
 
   for (let i = 0; i < quantity; i++) {
     const randomIndex = coreRandom(max)
-    const face = facesArr[randomIndex]
+    const face = faces[randomIndex]
     if (face) {
       result[i] = face
     }
