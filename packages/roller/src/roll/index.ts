81efea54cb5ab79803bbfb9cfae69c0ea2b6c686
alxjrvs@gmail.com <alxjrvs@gmail.com>
2025-07-19 03:13:02 -0400

    More tests, subtract

--- packages/roller/src/roll/index.ts
+++ packages/roller/src/roll/index.ts
@@ -5,7 +5,11 @@ import { generateRollRecord } from './generateRollRecord'
 export function roll(...args: RollArgument[]): RollerRollResult {
   const parameters = args.flatMap((arg) => argToParameter(arg))
   const rolls = parameters.map((parameter) => generateRollRecord(parameter))
-  const total = rolls.reduce((acc, cur) => acc + cur.total, 0)
+  const total = rolls.reduce((acc, cur) => {
+    const factor = cur.parameters.subtract ? -1 : 1
+    const total = cur.total * factor
+    return acc + total
+  }, 0)
   return {
     rolls,
     result: total,
