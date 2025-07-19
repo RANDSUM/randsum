f367839d51c1140b0a961eb0dbc58396689506a1
alxjrvs@gmail.com <alxjrvs@gmail.com>
2025-07-19 02:50:10 -0400

    Forward Thinking

--- packages/roller/src/roll/index.ts
+++ packages/roller/src/roll/index.ts
@@ -3,7 +3,7 @@ import { argToParameter } from './argToParameter'
 import { generateRollRecord } from './generateRollRecord'
 
 export function roll(...args: RollArgument[]): RollerRollResult {
-  const parameters = args.map((arg) => argToParameter(arg))
+  const parameters = args.map((arg) => argToParameter(arg)).flat()
   const rolls = parameters.map((parameter) => generateRollRecord(parameter))
   const total = rolls.reduce((acc, cur) => acc + cur.total, 0)
   return {
