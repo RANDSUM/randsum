7677c248d610b86902e47e62106b170f8b48d7e2
alxjrvs@gmail.com <alxjrvs@gmail.com>
2025-07-19 02:39:16 -0400

    WIP

--- packages/roller/__tests__/roll.test.ts
+++ packages/roller/__tests__/roll.test.ts
@@ -48,6 +48,26 @@ describe(roll, () => {
           })
         })
       })
+
+      describe('mixed args', () => {
+        const argOne = 20
+        const argTwo = { sides: 20 }
+        const argThree = '1d20'
+        test('it never goes outside of the bounds of the roll', () => {
+          const dummyArray = Array.from({ length: loops }, () =>
+            roll(argOne, argTwo, argThree)
+          )
+          dummyArray.forEach(({ total, rolls }) => {
+            expect(total).toBeLessThanOrEqual(60)
+            expect(total).toBeGreaterThanOrEqual(3)
+            expect(rolls).toHaveLength(3)
+            rolls.forEach((roll) => {
+              expect(roll.total).toBeLessThanOrEqual(20)
+              expect(roll.total).toBeGreaterThanOrEqual(1)
+            })
+          })
+        })
+      })
     })
   })
 
