1d85277c8e76d38dc65b2a12ee9a5fbba70d72a9
alxjrvs@gmail.com <alxjrvs@gmail.com>
2025-07-19 02:40:56 -0400

    Roll test

--- packages/roller/__tests__/roll.test.ts
+++ packages/roller/__tests__/roll.test.ts
@@ -10,13 +10,10 @@ describe(roll, () => {
       describe('numeric args', () => {
         const arg = 20
         test('it never goes outside of the bounds of the roll', () => {
-          const dummyArray = Array.from(
-            { length: loops },
-            () => roll(arg).total
-          )
-          dummyArray.forEach((individualRoll) => {
-            expect(individualRoll).toBeLessThanOrEqual(20)
-            expect(individualRoll).toBeGreaterThan(0)
+          const dummyArray = Array.from({ length: loops }, () => roll(arg))
+          dummyArray.forEach(({ total }) => {
+            expect(total).toBeLessThanOrEqual(20)
+            expect(total).toBeGreaterThan(0)
           })
         })
       })
@@ -24,13 +21,10 @@ describe(roll, () => {
       describe('object args', () => {
         const arg = { sides: 20 }
         test('it never goes outside of the bounds of the roll', () => {
-          const dummyArray = Array.from(
-            { length: loops },
-            () => roll(arg).total
-          )
-          dummyArray.forEach((individualRoll) => {
-            expect(individualRoll).toBeLessThanOrEqual(20)
-            expect(individualRoll).toBeGreaterThan(0)
+          const dummyArray = Array.from({ length: loops }, () => roll(arg))
+          dummyArray.forEach(({ total }) => {
+            expect(total).toBeLessThanOrEqual(20)
+            expect(total).toBeGreaterThan(0)
           })
         })
       })
@@ -38,13 +32,10 @@ describe(roll, () => {
       describe('notation args', () => {
         const arg = '1d20'
         test('it never goes outside of the bounds of the roll', () => {
-          const dummyArray = Array.from(
-            { length: loops },
-            () => roll(arg).total
-          )
-          dummyArray.forEach((individualRoll) => {
-            expect(individualRoll).toBeLessThanOrEqual(20)
-            expect(individualRoll).toBeGreaterThan(0)
+          const dummyArray = Array.from({ length: loops }, () => roll(arg))
+          dummyArray.forEach(({ total }) => {
+            expect(total).toBeLessThanOrEqual(20)
+            expect(total).toBeGreaterThan(0)
           })
         })
       })
@@ -68,29 +59,6 @@ describe(roll, () => {
           })
         })
       })
-    })
-  })
-
-  describe('corner cases', () => {
-    test('ordered options remain the same', () => {
-      const argsOne = {
-        sides: 1,
-        quantity: 2,
-        modifiers: {
-          plus: 5,
-          drop: { lowest: 1 }
-        }
-      }
-      const argsTwo = {
-        sides: 1,
-        quantity: 2,
-        modifiers: {
-          drop: { lowest: 1 },
-          plus: 5
-        }
-      }
-
-      expect(roll(argsOne).total).toEqual(roll(argsTwo).total)
     })
   })
 })
