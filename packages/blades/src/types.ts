2950363991cad31955675ae5a14f5470a9ae4b35
alxjrvs@gmail.com <alxjrvs@gmail.com>
2025-07-20 00:48:15 -0400

    Cleaner types

--- packages/blades/src/types.ts
+++ packages/blades/src/types.ts
@@ -1,14 +1,5 @@
 import type { RollResult } from '@randsum/roller'
 
-export type BladesCritical = 'critical'
-export type BladesSuccess = 'success'
-export type BladesPartial = 'partial'
-export type BladesFailure = 'failure'
-
-export type BladesResult =
-  | BladesCritical
-  | BladesSuccess
-  | BladesPartial
-  | BladesFailure
+export type BladesResult = 'critical' | 'success' | 'partial' | 'failure'
 
 export type BladesRollResult = RollResult<BladesResult>
