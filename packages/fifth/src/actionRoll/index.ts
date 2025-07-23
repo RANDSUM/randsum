fc8f8dfd09cf4d1f14efebf5c4a5ee3340774f4f
Alex Jarvis <alxjrvs@users.noreply.github.com>
2025-07-22 20:12:56 -0400

    Remove wrapper.  (#830)

--- packages/fifth/src/actionRoll/index.ts
+++ packages/fifth/src/actionRoll/index.ts
@@ -1,16 +1,12 @@
-import { type RollerRollResult, rollWrapper } from '@randsum/roller'
+import { type RollerRollResult, roll } from '@randsum/roller'
 import type { FifthRollArgument } from '../types'
 import { generateQuantity } from './generateQuantity'
 import { generateModifiers } from './generateModifiers'
 
-const actionRoll: (arg: FifthRollArgument) => RollerRollResult = rollWrapper({
-  toArg: (arg: FifthRollArgument) => [
-    {
-      sides: 20,
-      quantity: generateQuantity(arg.rollingWith),
-      modifiers: { ...generateModifiers(arg.rollingWith), plus: arg.modifier }
-    }
-  ]
-})
-
-export { actionRoll }
+export function actionRoll(arg: FifthRollArgument): RollerRollResult {
+  return roll({
+    sides: 20,
+    quantity: generateQuantity(arg.rollingWith),
+    modifiers: { ...generateModifiers(arg.rollingWith), plus: arg.modifier }
+  })
+}
